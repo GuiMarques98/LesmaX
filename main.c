3180137d2e414428f0edcaea41f9b0caa40e644e
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2016-11-12 15:46:11 -0200

    Função de soma

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soma(int a, int b);
+
 int main()
 {
     printf("Hello world!\n");
     return 0;
 }
 
-
+int soma(int a, int b)
+{
+    return (a+b);
+}
