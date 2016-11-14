fb6e392f9631ba2756a4ceb36ed179201eacf3a1
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2016-11-14 18:24:24 -0200

    Função principal com a estrutura do projeto

--- main.c
+++ main.c
@@ -1,26 +1,100 @@
+/*
+Projeto de PDS Grupo 3
+Membros:
+	Guilherme Marques Moreira da Silva	
+	Gustavo 
+	Gustavo
+*/
+//Includes
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+#include <locale.h>
 
-int soma(int a, int b);
+//Defines
+#define ATORMAXNOME 30
+#define ATORMAXFILMES 200
+#define ATORMAXNACIONALIDADE 20
 
+//Structs
+
+//Struct para atores
+typedef struct{
+	char nome[ATORMAXNOME];
+	int matricula;
+	int idade;
+	int profissao;
+	char filmes_feitos[ATORMAXFILMES];
+	char nacionalidade[ATORMAXNACIONALIDADE];
+}Atores;
+
+//Cabeçalhos
+
+
+//Principal
 int main()
 {
-    printf("Hello world!\n");
+	//Declarações 
+	char opcao;
+	int validaOpcaoMain(int opcao);
+	
+	//Instruções
+	setlocale(SET_ALL, "");
+	
+	do
+	{
+		printf("PRINCIPAL\n______________________________________________________________________\n\n");
+		do
+		{
+			printf("\nEscolha o menu que deseja entrar:\n1-Filmes\n2-Salas de Cinema\n3-Atores\nInsira a opção:");
+			opcao = getch();
+			
+		}while(!validaOpcaoMain(opcao));
+		
+		
+		switch(opcao)
+		{
+			case '1':
+				
+				break;
+			case '2':
+				
+				break;
+			case '3':
+				
+				break;
+		}
+		
+	}while(opcao != 0);
+	
     return 0;
 }
 
-int soma(int a, int b)
+//================================================SUBPROGRAMAS==================================================
+//=================================MENU PRINCIPAL====================================
+//Objetivo : Validar opção do menu principaç 
+//Parametro: Opção 
+//Retorno  : Situação da validação 
+int validaOpcaoMain(int opcao)
 {
-    return (a+b);
+	//Instruções 
+	
+	if(toupper(opcao) != 83 && toupper(opcao) != 78)
+	{
+		printf("Opção inválida!\n");
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
+//====================================ATORES=========================================
 
-int subtracao(int a, int b){
-	return (a-b);
-}
 
-int multiplicacao(int a, int b)
-{
-	return (a*b);
+//===================================FILMES==========================================
 
-}
+
+//===================================SALAS DE CINEMA ================================
